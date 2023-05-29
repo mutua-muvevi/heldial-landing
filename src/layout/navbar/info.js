3b2723e7739b5c2caa3e61103dfc0e6a4f8a800a
samdev <samstore637@gmail.com>
2023-05-29 21:55:36 +0300

    first upload for nav and footer

---
+++ src/layout/navbar/info.js
@@ -0,0 +1,42 @@
+
+export const menuItems = [
+	{
+		label: "Home",
+		path: "/landing/home"
+	},
+	{
+		label: "About",
+		path: "/landing/about"
+	},
+	{
+		label: "Contact",
+		path: "/landing/contact"
+	},
+]
+
+export const drawerMenuItems = {
+	top: [
+		{
+			label: "Home",
+			path: "/landing/home"
+		},
+		{
+			label: "About",
+			path: "/landing/about"
+		},
+		{
+			label: "Contact",
+			path: "/landing/contact"
+		},
+	],
+	bottom: [
+		{
+			label: "Login",
+			link: ""
+		},
+		{
+			label: "Register",
+			link: ""
+		},
+	]
+}
